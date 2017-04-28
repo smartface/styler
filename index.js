a28499a616dea6324f6ff34e3d2313e925b22591
Cenk Cetinkaya <mail@cenkcetinkaya.com>
2017-04-28 10:36:51 +0000

    Add index

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    styler: require("./dist/styler"),
+    componentStyler: require("./dist/componentStyler"),
+    extendStyler: require("./dist/extendStyler"),
+    mergeStyles: require("./dist/mergeStyles"),
+    themeStyler: require("./dist/themeStyler"),
+    memoizeStyler: require("./dist/memoizeStyler"),
+};
