c9a0edd1375c6101632abedcd47d1c059b362a08
Cenk Cetinkaya <mail@cenkcetinkaya.com>
2017-05-03 14:03:29 +0000

    struct(styler): add new commit type in the cz-config
    Add new commit type in the cz-config

--- .cz-config.js
+++ .cz-config.js
@@ -13,12 +13,17 @@ module.exports = {
   types: [
     {value: 'feat',     name: 'feat:     Add a new feature'},
     {value: 'fix',      name: 'fix:      Submit a bug fix'},
-    {value: 'refactor', name: 'refactor: A code change that neither fixes a bug nor adds a feature.\n            Includes code style changes.'},
+    {value: 'refactor', name: 'refactor: A code change that neither fixes a bug nor adds a feature.\
+    Includes code style changes.'
+    },
     {value: 'test',     name: 'test:     Add tests only'},
     {value: 'docs',     name: 'docs:     Documentation only changes'},
     {value: 'release',  name: 'release:  Publish a new version of a package.'},
-    {value: 'chore',    name: 'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation. META only.'},
+    {value: 'chore',    name: 'chore:    Changes to the build process or auxiliary tools\
+    and libraries such as documentation generation. META only.'
+    },
     {value: 'perf',     name: 'perf:     A code change that improves performance'},
+    {value: 'struct',   name: 'struct:   A folder structures change that improves development.'},
   ],
 
   scopes: packageScopes.concat(otherScopes)
