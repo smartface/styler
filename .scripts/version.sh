db749b7571af36a75f881c7ead761abb9eb59449
Cenk Cetinkaya <mail@cenkcetinkaya.com>
2017-05-05 07:17:38 +0000

    chore(CI): improve version.sh
    Improve version.sh

--- .scripts/version.sh
+++ .scripts/version.sh
@@ -1,20 +1,28 @@
 #!/usr/bin/env bash
 
-version=$(cat package.json | jase version)
+NEW_APP_VERSION=$(cat package.json | jase NEW_APP_VERSION)
 
 type=$1
+exitstatus=0
 
 if [ -f $type ] ; then
-    version=$(node ./.scripts/incVersion.js -v $version);
+    NEW_APP_VERSION=$(node ./.scripts/incNEW_APP_VERSION.js -v $NEW_APP_VERSION || exitstatus=$?);
 else
     if [[ $type == "minor" || $type == "major" || $type == "patch" ]] ; then
-        version=$(node ./.scripts/incVersion.js -v $version --$type);
+        NEW_APP_VERSION=$(node ./.scripts/incNEW_APP_VERSION.js -v $NEW_APP_VERSION --$type || exitstatus=$?);
     fi
 fi
 
-if [ -f $version ] ; then
-    echo "no version found";
-else
-    result=$(cat package.json | jase version -s $version > package2.json);
+if [ -f $NEW_APP_VERSION ] ; then
+    echo "no NEW_APP_VERSION found";
+elif [ $exitstatus -ne 1 ]; then
+    # back up current
+    cat package.json > package.old.json
+    # change current NEW_APP_VERSION
+    result=$(cat package.json | jase NEW_APP_VERSION -s $NEW_APP_VERSION > package2.json);
+    # replace with new one
     mv package2.json package.json;
+elif [$exitstatus -ne 0]; then
+    break;
+    exit $exitstatus
 fi
---
+++ CHANGELOG.md
@@ -0,0 +1,30 @@
+<a name="1.0.0-rc.3"></a>
+# [1.0.0-rc.3](https://github.com/smartface/styler/compare/v1.0.0-rc3...v1.0.0-rc.3) (2017-05-04)
+
+
+### Bug Fixes
+
+* **styler:** fix node version in travis.yml ([b871062](https://github.com/smartface/styler/commit/b871062))
+
+
+### BREAKING CHANGES
+
+* **styler:** Fix node version in travis.yml
+
+
+
+<a name="1.0.0-rc.1"></a>
+# [1.0.0-rc.1](https://github.com/smartface/styler/compare/v1.0.0-rc3...v1.0.0-rc.1) (2017-05-04)
+
+
+### Bug Fixes
+
+* **styler:** fix node version in travis.yml ([b871062](https://github.com/smartface/styler/commit/b871062))
+
+
+### BREAKING CHANGES
+
+* **styler:** Fix node version in travis.yml
+
+
+
