8a937a236aa8e81797f295b6b82df9d9f19f812b
Cenk Cetinkaya <mail@cenkcetinkaya.com>
2017-05-10 10:54:35 +0000

    test(styler): add style merging tests
    Add style merging tests

--- src/utils/merge.js
+++ src/utils/merge.js
@@ -3,20 +3,23 @@ function isObj(val){
 }
 
 function recurse(acc, obj){
-  const output = {};
-  
   for (var p in obj) {
-    acc[p] = acc[p] || {};
-    acc[p] = isObj(obj[p]) ? recurse(acc[p], obj[p]) : obj[p];
+    acc[p] = isObj(obj[p]) ? recurse(acc[p] || {}, obj[p]) : obj[p];
   }
   
   return acc;
 }
 
+/**
+ * Creates deep copy and given merge objects
+ * 
+ */
 export default function deepMerge() {
-  var args = Array.prototype.slice.call(arguments);
+  let acc = {};
   
-  return args.reduce((acc, curr) => {
-    return recurse(acc, curr);
-  }, {});
+  for (var i=0; i < arguments.length; i++) {
+    acc = recurse(acc, arguments[i]);
+  }
+  
+  return acc;
 }
--- test/merge.test.js
+++ test/merge.test.js
@@ -81,6 +81,25 @@ const basicStyle2 = {
 
 
 describe("Merging Styles", function() {
+  it("should create deeply copy of style", function() {
+    const basicStyle1 = {
+      ".button": {
+        width: 100,
+        height: 200,
+        ".red": {
+          color: "red1",
+        }
+      },
+    };
+    
+    let style = merge(basicStyle1);
+
+    expect(style).to.be.eql(basicStyle1);
+    expect(style !== basicStyle1).to.be.true;
+    expect(style[".button"] === basicStyle1[".button"]).to.be.false;
+    expect(style[".button"][".red"] === basicStyle1[".button"][".red"]).to.be.false;
+  });
+
   it("should merge deeply two styles as a new instance", function() {
     let style = merge(basicStyle1, basicStyle2);
     expect(style).to.be.eql({ '.button': { width: 100, height: 200, '.red': { color: 'red1' } } });
@@ -128,7 +147,7 @@ describe("Merging Styles", function() {
     });
   });
   
-  it("should merge deeply 3 styles as a new instance", function() {
+  it("should merge deeply 3 and more styles as a new instance", function() {
     
   });
 });
