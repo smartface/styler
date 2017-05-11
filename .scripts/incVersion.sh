cf409d8cc488e7839af5faeec5ccba174de9bd76
Cenk Cetinkaya <mail@cenkcetinkaya.com>
2017-05-11 10:53:26 +0000

    fix(CI): fix version stepper
    Fix version stepper

--- .scripts/incVersion.sh
+++ .scripts/incVersion.sh
@@ -29,8 +29,8 @@ elif [ $exitstatus -ne 1 ]; then
     # replace with new one
     mv package2.json package.json;
     git add .
-    git tag v$NEW_APP_VERSION || $exitstatus=$?;
     git commit -m "release(styler): v$NEW_APP_VERSION" || $exitstatus=$?;
+    git tag v$NEW_APP_VERSION || $exitstatus=$?;
 elif [$exitstatus -ne 0]; then
     break;
     exit $exitstatus
