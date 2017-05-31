e17ad64c27973779598870ece02692809063d506
Cenk Cetinkaya <mail@cenkcetinkaya.com>
2017-05-31 15:32:19 +0000

    refactor(styler): clean
    clean

--- src/utils/flatMapStyles.js
+++ src/utils/flatMapStyles.js
@@ -1,15 +1,7 @@
 import merge from "./merge";
 
 const flatMapStyles = function(style, classNames, fn) {
-  // const classNamesCopy = classNames.slice();
-  // const className = classNamesCopy.shift();
-  // let parent = "";
 
-  // console.log("className", style, className);
-  // if (Array.isArray(className)) {
-  //   mapStyles(style, className, fn);
-  // } else if (typeof style[className] === 'object' && classNamesCopy) {
-    // parent = className;
   var mergedStyle = {};
   var styles = [];
 
@@ -19,12 +11,7 @@ const flatMapStyles = function(style, classNames, fn) {
   
   mergedStyle = merge.call(null, styles);
   
-  console.log(mergedStyle);
-  
   fn(classNames, mergedStyle);
-
-    // mapStyles(style[className], classNamesCopy, fn);
-  // }
 };
 
 export default flatMapStyles;
